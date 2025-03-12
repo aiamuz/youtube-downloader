12976e8b8c9ca1736cf554e798fb725a09585cbf
aiamuz <aiamuz.media@gmail.com>
2025-03-11 18:02:29 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python3 server.py
+
+# حذف ویدیوهای قدیمی‌تر از 6 ساعت
+find /app/downloads -type f -mmin +360 -exec rm -f {} \;
+
+# اجرای Gunicorn به جای Python مستقیم
+gunicorn --bind 0.0.0.0:5000 server:app
